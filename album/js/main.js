9a0896bb623267149cac2e05f4dd4b589910dc34
mikum <mikumkf@outlook.com>
2016-07-11 19:51:16 +0800

    Finish anima

--- album/js/main.js
+++ album/js/main.js
@@ -25,7 +25,46 @@ var headersData = [
     }
 ];
 
+var options = {
+    stackItemsAnimation: {
+        duration: 800,
+        type: dynamics.spring
+    },
+    stackItemsPreAnimation: {
+        accept: {
+            elastic: true,
+            animationProperties: {translateX: 100, translateY: 10, rotateZ: 5},
+            animationSettings: {
+                duration: 100,
+                type: dynamics.easeIn
+            }
+        },
+        reject: {
+            elastic: true,
+            animationProperties: {translateX: -100, translateY: 10, rotateZ: -5},
+            animationSettings: {
+                duration: 100,
+                type: dynamics.easeIn
+            }
+        }
+    }
+};
+
 (function (window) {
+    var animaStack = function () {
+        console.log("K");
+        var Bleft = document.getElementById('button_left'),
+            Bright = document.getElementById('button_right'),
+            stack = new Stack(document.getElementById('stackAnima'), options);
+        Bleft.addEventListener("click", function () {
+            stack.reject()
+        });
+        Bright.addEventListener("click", function () {
+            stack.accept()
+        });
+        window.stack = this.stack;
+    };
+
     Vue.http.options.emulateJSON = true;
     var header = new Vue({
         el: 'header',
@@ -39,28 +78,31 @@ var headersData = [
             this.$http.get('db/album.json').then(function (response) {
                 this.$set('photos', response.json())
             });
+            setTimeout(animaStack, 2000);
         },
         data: {
             'photos': ''
         }
     });
-    var mouseOn = function () {
-        var header = document.getElementById('header'),
-            mouseHandler = function (event) {
-                if (event.wheelDelta < 0) {
-                    header.style.display = "none";
-            }
-        };
-        document.body.onmousewheel = function (event) {
-            event = event || window.event;
-            mouseHandler(event);
-        };
-        document.body.addEventListener("DOMMouseScroll", function (event) {
-            mouseHandler(event);
-        });
-    };
-    mouseOn();
-})(window);
 
+    // var mouseOn = function () {
+    //     var header = document.getElementById('header'),
+    //         addWheel = 0,
+    //         mouseHandler = function (event) {
+    //             addWheel += event.wheelDelta;
+    //             if (addWheel < -240){
+    //
+    //             }
+    //         };
+    //     document.body.onmousewheel = function (event) {
+    //         event = event || window.event;
+    //         mouseHandler(event);
+    //     };
+    //     document.body.addEventListener("DOMMouseScroll", function (event) {
+    //         mouseHandler(event);
+    //     });
+    // };
+    // mouseOn();
+})(window);
 
 
