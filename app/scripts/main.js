036c33d4c7c6618596df1ea92b1a12c3f2d56178
MKFMIKU <mikumkf@outlook.com>
2016-09-17 18:11:31 +0800

    ADD MENU

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,23 @@
 window.onload=function () {
-  var LogoDom = document.getElementById("logo");
+  var LogoDom = document.getElementById("logo"),
+    honorButton = document.getElementById("honorButton"),
+    honorBox = document.getElementById("honorBox"),
+    closeB = document.getElementById('close'),
+    closeB1 = closeB.children[0],
+    closeB2 = closeB.children[1];
+
   LogoDom.classList.add("fadeIn");
+
+  honorButton.addEventListener('click', function () {
+    honorBox.classList.add('showBox');
+    closeB1.className = 'barOne';
+    closeB2.className = 'barTwo';
+  });
+
+  closeB.addEventListener('click', function() {
+    honorBox.classList.remove('showBox');
+    closeB1.className = "";
+    closeB2.className = "";
+  });
+
 };
