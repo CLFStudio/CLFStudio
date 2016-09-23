36ae6c1600dd66b8eb1a1d965ca556d1a0126d97
MKFMIKU <mikumkf@outlook.com>
2016-09-23 11:36:43 +0800

    For mobile

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,16 +1,16 @@
 window.onload=function () {
-  var LogoDom = document.getElementById("logo"),
-    nav = document.getElementById("nav"),
-    menu = document.getElementById("menu"),
-    honorButton = document.getElementById("honorButton"),
-    honorBox = document.getElementById("honorBox"),
+  var LogoDom = document.getElementById('logo'),
+    nav = document.getElementById('nav'),
+    menu = document.getElementById('menu'),
+    honorButton = document.getElementById('honorButton'),
+    honorBox = document.getElementById('honorBox'),
     closeB = document.getElementById('close'),
     project = document.getElementById('project'),
     closeB1 = closeB.children[0],
     closeB2 = closeB.children[1],
     menuStatus = 0;
 
-  LogoDom.classList.add("fadeIn");
+  LogoDom.classList.add('fadeIn');
 
   honorButton.addEventListener('click', function () {
     honorBox.classList.add('showBox');
@@ -20,20 +20,20 @@ window.onload=function () {
 
   closeB.addEventListener('click', function() {
     honorBox.classList.remove('showBox');
-    closeB1.className = "";
-    closeB2.className = "";
+    closeB1.className = '';
+    closeB2.className = '';
   });
 
   menu.addEventListener('click', function () {
     if(menuStatus){
       nav.classList.remove('show-nav');
       menuStatus=0;
-      document.getElementById('body').style.overflowY="scroll";
+      document.getElementById('body').style.overflowY='scroll';
     }
     else{
       nav.classList.add('show-nav');
       menuStatus=1;
-      document.getElementById('body').style.overflowY="hidden";
+      document.getElementById('body').style.overflowY='hidden';
     }
   });
 
